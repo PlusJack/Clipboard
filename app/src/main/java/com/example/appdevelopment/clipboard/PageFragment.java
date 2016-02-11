480eb8b2b49ef295d2de16c5a81fec9f3ce4799f
Jack P <17jphillips@KENB083FEAA6B47.crcsd.abc>
2016-02-11 08:45:33 -0600

    Gahhh
    ughhhh

--- app/src/main/java/com/example/appdevelopment/clipboard/PageFragment.java
+++ app/src/main/java/com/example/appdevelopment/clipboard/PageFragment.java
@@ -1,14 +1,25 @@
 package com.example.appdevelopment.clipboard;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 // In this case, the fragment displays simple text based on the page
-public class PageFragment extends Fragment {
+public class PageFragment extends Activity {
+
     public static final String ARG_PAGE = "ARG_PAGE";
 
     private int mPage;
@@ -32,7 +43,13 @@ public class PageFragment extends Fragment {
                              Bundle savedInstanceState) {
         switch (mPage) {
             case 1:
-                View view1 = inflater.inflate(R.layout.recent_fragment, container, false);
+                ArrayList<String> clipboard = new ArrayList<>();
+                clipboard.add("Thing1");
+                clipboard.add("Thing2");
+                View view1 = inflater.inflate(R.layout.recent_fragment, null);
+                ListView listView = (ListView)view1.findViewById(R.id.recentList);
+                listView.setAdapter(new UserItemAdapter(PageFragment.this, R.layout.recent_item, clipboard));
+                ((ViewPager) collection).addView(view1, 0);
                 return view1;
             case 2:
                 View view2 = inflater.inflate(R.layout.starred_fragment, container, false);
@@ -41,9 +58,24 @@ public class PageFragment extends Fragment {
                 View view3 = inflater.inflate(R.layout.recent_fragment, container, false);
                 return view3;
         }
+
         /*View view = inflater.inflate(R.layout.recent_fragment, container, false);
         TextView textView = (TextView) view;
         textView.setText("Fragment #" + mPage);
         return view;*/
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Recents, R.layout.recent_item);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/appdevelopment/clipboard/activityRecent.java
+++ app/src/main/java/com/example/appdevelopment/clipboard/activityRecent.java
@@ -4,11 +4,14 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Jack on 2/7/2016.
@@ -16,23 +19,24 @@ import android.widget.ListView;
 
 //Currently having issues dealing with ListViews within a fragment.
 
-public class activityRecent extends ListFragment {
+public class activityRecent extends ListFragment implements AdapterView.OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("activityRecent", "onCreateView has started.");
+        View view = inflater.inflate(R.layout.recent_fragment, container, false);
+        return view;
+    }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) { //Once the activity is created it sets these items to the list and defines the adapter used
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, values);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Recents, R.layout.recent_item);
         setListAdapter(adapter);
-
+        getListView().setOnItemClickListener(this);
     }
 
-    public void onViewCreated(View view, Bundle savedInstanceState) { //*should* connect the listed items and the adapter to the listRecent ListView in recent_fragment.xml
-        super.onViewCreated(view, savedInstanceState);
-        ListView list = (ListView) getView().findViewById(R.id.listRecent);
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
-
-    }
+}
